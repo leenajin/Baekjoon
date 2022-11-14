74dd225d282d34b24e8fa5c48193aef93e0f7598
leenajin <najin14@naver.com>
2022-11-14 12:26:59 +0900

    [Bronze V] Title: A-B, Time: 112 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        int A=0;
+        int B=0;
+        Scanner scn = new Scanner(System.in);
+        
+        A=scn.nextInt();
+        B=scn.nextInt();
+        
+        System.out.print(A-B);
+    }
+}
